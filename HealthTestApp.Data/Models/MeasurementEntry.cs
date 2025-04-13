aa6dc8661ce2f0364efc66a2e023760f05f5c46b
Yordan Yordanov <mixdata34@gmail.com>
2025-04-13 19:33:42 +0300

    Worked further on the Ruffier-Dickson test

---
+++ HealthTestApp.Data/Models/MeasurementEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTestApp.Data.Models
+{
+    public class MeasurementEntry
+    {
+        public string Systolic { get; set; } = string.Empty;
+        public string Diastolic { get; set; } = string.Empty;
+        public string HeartRate { get; set; } = string.Empty;
+    }
+}
---
+++ HealthTestApp.Data/Models/RuffierDicksonTestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthTestApp.Data.Models
+{
+    public class RuffierDicksonTestData
+    {
+        public List<MeasurementEntry> Measurements { get; set; } = new();   
+        public List<string> Medians { get; set; } = new(Enumerable.Repeat("", 3));
+        public Dictionary<string, string> Outputs { get; set; } = new();
+        public RuffierDicksonTestData()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                //To pre-fill the List with 8 measurements.
+                Measurements.Add(new MeasurementEntry());
+            }
+        }
+
+    }
+}
