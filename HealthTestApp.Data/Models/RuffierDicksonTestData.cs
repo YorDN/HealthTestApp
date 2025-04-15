aa832ff4233fb96086380af9ed057d84b31c3174
Yordan Yordanov <mixdata34@gmail.com>
2025-04-15 19:52:06 +0300

    Added the 'backednd' logic of the test and now it works.

--- HealthTestApp.Data/Models/RuffierDicksonTestData.cs
+++ HealthTestApp.Data/Models/RuffierDicksonTestData.cs
@@ -11,12 +11,26 @@ namespace HealthTestApp.Data.Models
     {
         public List<MeasurementEntry> Measurements { get; set; } = new();   
         public List<double> Medians { get; set; } = new(Enumerable.Repeat(0.0, 3));
+        public List<double> BloodPressureMedians { get; set; } = new( Enumerable.Repeat(0.0, 8));
+        public List<double> PulsePressures { get; set; } = new(Enumerable.Repeat(0.0, 8));
+        public List<double> EjectedBloodMasses { get; set; } = new(Enumerable.Repeat(0.0, 8));
+        public List<CardiacDuration> CardiacDurations { get; set; } = new();
+        public List<double> ExternalWorks { get; set; } =  new(Enumerable.Repeat(0.0, 8));
+        public List<double> KineticWorks { get; set; } = new(Enumerable.Repeat(0.0, 8));
+        public List<double> TotalWorks { get; set; } = new(Enumerable.Repeat(0.0, 8));
+        public List<double> AveragePowers { get; set; } = new(Enumerable.Repeat(0.0, 8));
         public Dictionary<string, double> Outputs { get; set; } = new();
+
         public RuffierDicksonTestData()
         {
             Measurements = Enumerable.Range(0, 8).Select(_ => new MeasurementEntry()).ToList();
-
+            CardiacDurations = Enumerable.Range(0, 8).Select(d => new CardiacDuration()).ToList();
         }
 
+    }
+    public class CardiacDuration()
+    {
+        public double CardiacCycleDurationSeconds { get; set; }
+        public double SystoleDurationSeconds { get; set; }
     }
 }
